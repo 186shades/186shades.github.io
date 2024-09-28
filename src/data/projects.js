a9110b9b62bde792af2137ac12b26f7d953225e5
Sharvari Deshmukh <sharvarisd18@gmail.com>
2024-09-28 00:16:20 -0700

    theme and style fixes

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,27 +3,17 @@ const data = [
   {
     title: 'Dust-Fragment classification @ UCSD ARCLab',
     subtitle: '',
-    image: '/images/projects/arclab.webp',
+    image: '/images/projects/DDNM.png',
     link: 'https://www.ucsdarclab.com/surgical-automation',
     date: '2024-04-1',
-    desc: 'Investigating recent representation learning algorithms to extract relevant features from ureteroscopy videos'
-    + 'Aiming to automate laser settings for lithotripsy procedure based on spatiotemporal detection of kidney stone, fragment and dust.',
+    desc: 'Computer Vision | Video Segmentation | Object Detection | Deep Learning',
   },
   {
     title: 'Histopathology image classification for cancer diagnosis',
     subtitle: '',
-    image: '/images/projects/lung_cancer.jpeg',
+    image: '/images/projects/Histopathology.png',
     date: '2024-02-1',
-    desc: 'Conducted comparative analysis between CNNs & Vision Transformers Fine-tuning both models through transfer learning & data augmentation.'
-    + 'Implemented fine-grained architectural modifications attaining 99% accuracy with API-Net & Swin-Transformers on LC25000 dataset.',
-  },
-  {
-    title: 'EEG-Based Sleep Analysis',
-    subtitle: '',
-    image: '/images/projects/eeg.png',
-    date: '2016-10-28',
-    desc:
-      'Designed elliptic filters for EEG signal processing, enabling the detection of sleep stages based on distinct brainwave frequency bands.',
+    desc: 'Computer Vision | Image Classification | Deep Learning | Transfer Learning | Transformers',
   },
 ];
 
